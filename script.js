e5e541d548097a632d6646b9d682dbad03487757
Jurgen <jurgen.gjoncari@gmail.com>
2021-06-22 18:06:56 +0200

    Render the input to the table

--- script.js
+++ script.js
@@ -5,35 +5,52 @@ function Book(title, author, pages, read) {
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	this.info = function () {
-		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-	}
+	// this.info = function () {
+	//	return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+	// }
 }
 
-function addBookToLibrary() {
-	book = prompt();
-	myLibrary.append(book);
+let book0 = new Book("Dan Brown", "The Da Vinci Code", 359, "Yes");
+let book1 = new Book("Khaled Hosseini", "The Kite Runner", 371, "Yes");
+
+function addBookToLibrary(book) {
+	myLibrary.push(book);
 }
+
+addBookToLibrary(book0);
+addBookToLibrary(book1);
+
+// console.log(myLibrary);
+
+let table = document.querySelector("tbody");
+
+let inputs = document.querySelectorAll("input");
 
 function displayBookOnPage() {
 	for (book of myLibrary) {
-		// display it in a table?? or their own card??
+		let row = table.insertRow();
+
+		for (value of Object.values(book)) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(String(value));
+			cell.appendChild(text);
+		}
 	}
 }
 
-let button = document.querySelector("button");
-// button.addEventListener("click", function () {
-	let table = document.querySelector("tbody"); 
-	
-	let row = table.insertRow();
+displayBookOnPage();
 
-	let inputs = document.querySelectorAll("input");
+let button = document.querySelector("form button");
 
-	for (input of inputs) {
-		input.addEventListener("click", function () {
-			let cell = row.insertCell();
+button.addEventListener("click", function () {
+	let author = document.querySelector("#author").value;
+	let title = document.querySelector("#title").value;
+	let pages = document.querySelector("#pages").value;
+	let read = document.querySelector("[name=read]").value;
 
-			let answer = document.createTextNode(input.value);
-			cell.appendChild(answer);
-		})
-	}
+	let book = new Book(author, title, pages, read);
+
+	addBookToLibrary(book);
+
+	displayBookOnPage();
+})
